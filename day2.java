1672786a6a9ffc51a41c27cc3c151e1c8d2f228a
NurpadilaD0221121 <99808386+NurpadilaD0221121@users.noreply.github.com>
2022-02-20 21:57:44 +0800

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,17 @@
+
+package tugas;
+
+public class day2 {
+
+  public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  int a = scan.nexInt();
+  int b = scan.nexInt();
+  int c = scan.nexInt();
+  
+  System.out.println(a);
+  System.out.println(b);
+  System.out.println(c);
+   
+  }
+}
